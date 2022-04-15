e53f9ecafa24affb59141439d8caf886b5d4a4fd
Brandon Benoit <b.nutty@gmail.com>
2022-04-15 13:48:17 -0700

    completed portfolio section

---
+++ src/data.js
@@ -0,0 +1,187 @@
+export const headers = [
+	{
+		id: 'featured',
+		title: 'Featured',
+	},
+	{
+		id: 'web',
+		title: 'Web App',
+	},
+	{
+		id: 'mobile',
+		title: 'Mobile App',
+	},
+	{
+		id: 'design',
+		title: 'Design',
+	},
+	{
+		id: 'branding',
+		title: 'Branding',
+	},
+];
+
+export const featured = [
+	{
+		id: 1,
+		title: 'Featured',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 2,
+		title: 'Featured',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 3,
+		title: 'Featured',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 4,
+		title: 'Featured',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 5,
+		title: 'Featured',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 6,
+		title: 'Featured',
+		img: '/jacobs-world.jpg',
+	},
+];
+
+export const web = [
+	{
+		id: 1,
+		title: 'Web App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 2,
+		title: 'Web App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 3,
+		title: 'Web App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 4,
+		title: 'Web App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 5,
+		title: 'Web App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 6,
+		title: 'Web App',
+		img: '/jacobs-world.jpg',
+	},
+];
+
+export const mobile = [
+	{
+		id: 1,
+		title: 'Mobile App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 2,
+		title: 'Mobile App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 3,
+		title: 'Mobile App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 4,
+		title: 'Mobile App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 5,
+		title: 'Mobile App',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 6,
+		title: 'Mobile App',
+		img: '/jacobs-world.jpg',
+	},
+];
+
+export const design = [
+	{
+		id: 1,
+		title: 'Design',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 2,
+		title: 'Design',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 3,
+		title: 'Design',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 4,
+		title: 'Design',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 5,
+		title: 'Design',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 6,
+		title: 'Design',
+		img: '/jacobs-world.jpg',
+	},
+];
+
+export const branding = [
+	{
+		id: 1,
+		title: 'Brand',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 2,
+		title: 'Brand',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 3,
+		title: 'Brand',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 4,
+		title: 'Brand',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 5,
+		title: 'Brand',
+		img: '/jacobs-world.jpg',
+	},
+	{
+		id: 6,
+		title: 'Brand',
+		img: '/jacobs-world.jpg',
+	},
+];
